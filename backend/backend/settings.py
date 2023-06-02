f64b89038d9d12ba9afc97509e7890edbc41eb6e
Alphonse Kaman <kamanalphonse@gmail.com>
2023-06-02 11:49:49 +0000

    first model designed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my own app
+    'message',
 ]
 
 TIME_ZONE = 'Africa/Abidjan'
