8f989f0b91460523a5e7988465ce909fc04b7edf
Alphonse Kaman <kamanalphonse@gmail.com>
2023-06-15 14:43:42 +0000

    Testing expertises and footer components

--- frontend/src/components/Expertises.test.jsx
+++ frontend/src/components/Expertises.test.jsx
@@ -3,10 +3,6 @@ import { render, screen } from '@testing-library/react' ;
 import Expertises from './Expertises';
 
 
-function sum(a, b) {
-   return a + b;
-}
  test("Testing the expertises component: ", () => {
     render(<Expertises />);
-    expect(sum(5, 8)).toEqual(13);
  })
--- frontend/src/components/Footer.test.jsx
+++ frontend/src/components/Footer.test.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react' ;
 import '@testing-library/jest-dom';
-// import Footer from './Footer';
+import Footer from './Footer';
 
 
-function sum(a, b) {
-   return a + b;
-}
  test("Testing the footer component: ", () => {
-   //  render(<Footer />);
-    expect(sum(5, 8)).toEqual(13);
+    render(<Footer />);
  })
