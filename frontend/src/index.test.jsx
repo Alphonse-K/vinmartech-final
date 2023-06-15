66dfa4393b7fa13d76339c3bb132b07410102c3d
Alphonse Kaman <kamanalphonse@gmail.com>
2023-06-15 17:11:25 +0000

    Testing the root component, index.js

---
+++ frontend/src/index.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react' ;
+import '@testing-library/jest-dom';
+import './index';
+
+ test("Testing the testimonials", () => {
+    render(
+        <index />
+    )
+ })
