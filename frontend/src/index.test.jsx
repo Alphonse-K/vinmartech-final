373c5af0bcefd490399685c75f80c37e570a3dfc
Alphonse Kaman <kamanalphonse@gmail.com>
2023-06-20 10:17:47 +0000

    still testing the components before dockerizing them

--- frontend/src/index.test.jsx
+++ frontend/src/index.test.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-// import { createRoot } from "react-dom/client";
-import '../__mocks__/intersectionObserverMock';
+import {render, screen} from '@testing-library/react';
+
+// import '../__mocks__/intersectionObserverMock';
 import ReactDOM from "react-dom/client";
 import App from './App';
+// import './index';
 
 jest.mock('react-dom/client', ()  => ({render: jest.fn(), createRoot: jest.fn()}) );
 
@@ -13,7 +15,11 @@ describe('Testing the root of the app', () => {
       div.id == 'root';
       document.body.appendChild(div);
       const root = ReactDOM.createRoot(document.getElementById('root'));
-      ReactDOM.createRoot(<App />);
-      expect(ReactDOM.createRoot).toHaveBeenCalledWith(<App />, div);
+      render(
+      <React.StrictMode>
+            <App />
+      </React.StrictMode>
+      );
+      expect(render).toHaveBeenCalledWith(<App />);
    })
 })
