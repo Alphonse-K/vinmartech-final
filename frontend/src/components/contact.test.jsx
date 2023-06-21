b65a6139ea98b747b82d22e7dafe275562371947
Alphonse Kaman <kamanalphonse@gmail.com>
2023-06-21 14:59:54 +0000

    improving the testing of components

--- frontend/src/components/contact.test.jsx
+++ frontend/src/components/contact.test.jsx
@@ -1,31 +1,19 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react' ;
+import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
 import Contact from './Contact';
 import ErrorBoundary from '../ErrorBoundry';
 import axios from 'axios';
 
 // jest.mock('axios');
+afterEach(cleanup);
 
 describe('Contact.jsx: ', () => {
 
-         function tree()  {
-            return  render(
-               <ErrorBoundary>
-                  <Contact />
-               </ErrorBoundary>
-               )
-            }
-
-
-   test('It should match with the snapshot', () => {
-
-      screen.debug();
-   })
-
-   test('Test button', () => {
-      const {getByPlaceholderText} = tree();
-      expect(screen.getByPlaceholderText('À votre écoute'))
+   test('Test button', async () => {
+      render(<ErrorBoundary><Contact /></ErrorBoundary>)
+      expect(await screen.findByRole('textbox')).toBeInTheDocument();
    })
    
 })
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css'
 import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,4 +12,6 @@ root.render(
       <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
 
